3b5d12b94cb543ece5cf33a03433e0e11dc08fa3
Eric Wong <eric.wong@monashmotorsport.com>
2022-10-29 21:07:50 +1100

    initial commit

---
+++ build/CMakeFiles/calibrate_stereo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibrate_stereo.dir/calib_stereo.cpp.o"
+  "calibrate_stereo"
+  "calibrate_stereo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibrate_stereo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/undistort_rectify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undistort_rectify.dir/undistort_rectify.cpp.o"
+  "undistort_rectify"
+  "undistort_rectify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undistort_rectify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
